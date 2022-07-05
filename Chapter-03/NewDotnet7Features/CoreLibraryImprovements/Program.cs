f165fd3cb59c57ee9ce06f4acd067f2b391145aa
Fiodar Sazanavets <f.sazanavets@gmail.com>
2022-07-05 18:44:26 +0100

    Add JSON features to the code samples.

--- Chapter-03/NewDotnet7Features/CoreLibraryImprovements/Program.cs
+++ Chapter-03/NewDotnet7Features/CoreLibraryImprovements/Program.cs
@@ -1,5 +1,9 @@
 ﻿using CoreLibraryImprovements;
 
 Console.WriteLine("Demonstrating improvements to time-related data types.");
-
 TimeDatatypeImprovements.DemoNewTimeFeatures();
+
+Console.WriteLine("Demonstrating improvements to JSON.");
+NewJsonFeatures.DemoJsonWriterOptions();
+NewJsonFeatures.ShowDefaultJsonSerializerOptions();
+NewJsonFeatures.DemoJsonPolymorphism();
