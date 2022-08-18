28a3d587087ff204254e6057f7a767045c6b81ee
Fiodar Sazanavets <f.sazanavets@gmail.com>
2022-08-18 16:53:16 +0100

    Add code samples for chapter 5.

---
+++ Chapter-05/EfCore7Demo/MvcDataApp/Data/FactoryManagerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcDataApp.Models;
+
+namespace MvcDataApp.Data;
+
+public class FactoryManagerContext : DbContext
+{
+
+    public FactoryManagerContext(DbContextOptions<FactoryManagerContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Factory> Factories { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<Shift> Shifts { get; set; }
+}
---
+++ Chapter-05/EfCore7Demo/MvcDataApp/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcDataApp.Models;
+
+public record Employee
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int EmployeeId { get; set; }
+    [StringLength(20)]
+    public string FirstName { get; set; }
+    [StringLength(20)]
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime StartDate { get; set; }
+    public int JobId { get; set; }
+
+    public Job Job { get; set; }
+    public ICollection<Shift> Shifts { get; set; }
+}
